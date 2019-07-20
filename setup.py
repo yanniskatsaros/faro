20fe9562f8416b62ce01f345043807d7d4620bff
Yannis Katsaros <32942328+yanniskatsaros@users.noreply.github.com>
2019-07-19 20:29:16 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='faro',
+      version='0.1',
+      description='An SQL-focused data analysis library for Python.',
+      url='http://github.com/yanniskatsaros/faro',
+      author='Yannis Katsaros',
+      author_email='yanniskatsaros@hotmail.com',
+      license='MIT',
+      packages=['faro'],
+      zip_safe=False)
