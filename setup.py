bced388772c4abb18237d458f7738ffab2663d69
Yannis Katsaros <yanniskatsaros@hotmail.com>
2019-07-28 00:27:52 -0500

    refactor code style

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='faro',
-      version='0.1',
-      description='An SQL-focused data analysis library for Python.',
-      url='http://github.com/yanniskatsaros/faro',
-      author='Yannis Katsaros',
-      author_email='yanniskatsaros@hotmail.com',
-      license='MIT',
-      packages=['faro'],
-      zip_safe=False)
+setup(
+    name='faro',
+    version='0.1',
+    description='An SQL-focused data analysis library for Python.',
+    url='http://github.com/yanniskatsaros/faro',
+    author='Yannis Katsaros',
+    author_email='yanniskatsaros@hotmail.com',
+    license='MIT',
+    packages=['faro'],
+    zip_safe=False
+)
