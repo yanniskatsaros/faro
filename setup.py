6d0c9bab59381e15874e78eb8c1475429d90474f
Yannis Katsaros <yanniskatsaros@hotmail.com>
2019-08-09 14:57:01 -0500

    update setup for distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+classifiers = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+]
+
+keywords = [
+    'sql', 'sqlite', 'sqlite3', 'database',
+    'data-science', 'data-analysis', 'python'
+]
 
 setup(
     name='faro',
-    version='0.1',
+    version='0.0.1',  # see https://semver.org
     description='An SQL-focused data analysis library for Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/yanniskatsaros/faro',
     author='Yannis Katsaros',
     author_email='yanniskatsaros@hotmail.com',
     license='MIT',
-    packages=['faro'],
-    zip_safe=False
+    packages=find_packages(),
+    install_requires=['pandas'],
+    zip_safe=False,
+    classifiers=classifiers,
+    keywords=' '.join(keywords)
 )
