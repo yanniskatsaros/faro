3cd57e6922fa0945ceadfe6a3a09f72ea0393eec
Yannis Katsaros <yanniskatsaros@hotmail.com>
2020-05-25 21:27:01 -0500

    :construction: Add @table decorator implementation

--- faro/table.py
+++ faro/table.py
@@ -1,4 +1,21 @@
-from typing import List, Tuple
+import inspect
+from datetime import date, datetime
+from typing import List, Tuple, Type, Type, Optional
+
+from pydantic import create_model
+from pydantic.fields import ModelField
+
+# see https://www.sqlite.org/datatype3.html
+# 3.1.1 Affinity Name Examples
+SQLITE_TYPEMAP = {
+    str: 'TEXT',
+    float: 'REAL',
+    int: 'INTEGER',
+    date: 'DATE',
+    datetime: 'DATETIME',
+    bool: 'NUMERIC',
+    None: 'BLOB',
+}
 
 class Table:
     def __init__(self, data : List[Tuple], header=True, columns=None):
@@ -13,8 +30,6 @@ class Table:
         else:
             self.columns = [str(i) for i in range(self.n_cols)]
             self.data = [tuple(row) for row in data]
-
-
 
         # overrides the column names
         if columns:
@@ -66,3 +81,98 @@ class Table:
         """
         from pandas import DataFrame  # type: ignore
         return DataFrame(self.to_dict())
+
+def table(cls: Type, name: Optional[str]=None):
+    """
+    `dataclass` style decorator for easily defining
+    table schema using type hints and default values.
+
+    Parameters
+    ----------
+    cls: Type
+        The class definition to decorate
+
+    name: Optional[str] = None
+        Optionally override the default name of the
+        decorated class name. If `None` is provided
+        it will use `cls.__name__.lower()` for the
+        name of the table in the schema.
+
+    Returns
+    -------
+    cls
+
+    """
+    if not inspect.isclass(cls):
+        got_type = type(cls)
+        raise TypeError(f'Expected input type: class, got: {got_type}')
+
+    # TODO - refer back to https://github.com/samuelcolvin/pydantic/issues/153#issuecomment-633708838
+    # clean attribute names with trailing underscore such as `from_` or `int_` for
+    # any fields that clash with Python keywords
+    # potentially use a `normalize: Optional[bool] = False` kwarg to handle this behavior?
+
+    ann: dict = cls.__annotations__
+    cls_map: dict = cls.__dict__
+    signature = {a: (t, cls_map.get(a, ...)) for a, t in ann.items()}
+
+    cls = create_model(cls.__name__, **signature)
+
+    # use the input class name for the underlying table name
+    if not name:
+        name: str = cls.__name__.lower()
+
+    @staticmethod
+    def sql_schema() -> str:
+        """
+        Generates a SQLite3 compatible table schema
+        that can be used to create a table in a database.
+
+        Returns
+        -------
+        sql: str
+            Table schema definition
+
+        """
+        def field_to_column(f: ModelField) -> str:
+            REQUIRED = {
+                True: 'NOT NULL',
+                False: f'DEFAULT {f.default}'
+            }
+            return f'{f.name} {SQLITE_TYPEMAP[f.type_]} {REQUIRED[f.required]}'
+        
+        column_defs = [field_to_column(f) for f in cls.__fields__.values()]
+        schema = ',\n'.join('    ' + cdf for cdf in column_defs)
+
+        return f'CREATE TABLE IF NOT EXISTS {name} (\n{schema}\n)'    
+
+    @staticmethod
+    def sql_insert() -> str:
+        """
+        Generates a SQLite3 compatible SQL insert statement
+        using the model fields and types.
+
+        Returns
+        -------
+        sql: str
+            Insert statement
+        
+        """
+        cols: str = ', '.join(f.name for f in cls.__fields__.values())
+        wildcards: str = ', '.join('?' for _ in range(len(cls.__fields__)))
+        
+        return f'INSERT INTO {name}({cols}) VALUES ({wildcards})'
+
+    @staticmethod
+    def sql_drop() -> str:
+        return f'DROP TABLE {name} IF NOT EXISTS'
+
+    return type(
+        cls.__name__,
+        (cls, ),
+        {
+            'sql_schema': sql_schema,
+            'sql_insert': sql_insert,
+            'sql_drop': sql_drop
+        }
+    )
